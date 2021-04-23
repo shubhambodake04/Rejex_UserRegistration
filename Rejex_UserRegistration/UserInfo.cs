c00870969133273efeaa770ffc99cab24a033734
shubhambodake04 <shubhambodake6476@gmail.com>
2021-04-23 16:32:32 +0530

    Added in master

--- Rejex_UserRegistration/UserInfo.cs
+++ Rejex_UserRegistration/UserInfo.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Rejex_UserRegistration
 {
     class UserInfo
     {
+        string FirstName = "^[A-Z][a-z]{2,}$";
+
+        public bool validateUserInfo(string input)
+        {
+            return Regex.IsMatch(input, FirstName);
+        }
     }
 }
