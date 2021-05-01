1c726d73f33d79c0c5e5b462b63efa13e9583602
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-01 12:40:52 +0530

    [Shubham] Added in master

--- Rejex_UserRegistration/Program.cs
+++ Rejex_UserRegistration/Program.cs
@@ -9,6 +9,19 @@ namespace Rejex_UserRegistration
             UserInfo userinfo = new UserInfo();
             bool result = userinfo.ValidateFirstName("Shubham");
             Console.WriteLine(result);
+            bool result1 = userinfo.ValidateLastName("Bodake");
+            Console.WriteLine(result1);
+            bool result2 = userinfo.ValidateEmail("Shubham@gmail.com");
+            Console.WriteLine(result2);
+            bool result3 = userinfo.ValidateMobileNumber("91 7757929383");
+            Console.WriteLine(result3);
+            bool result4 = userinfo.ValidatePassword("shubhambodake");
+            Console.WriteLine(result4);
+            bool result5 = userinfo.ValidatePasswordRule2("Shubhambodake");
+            Console.WriteLine(result5);
+            bool result6 = userinfo.ValidatePasswordRule3("Shubham@123");
+            Console.WriteLine(result6);
+
             Console.ReadKey();
         }
     }
