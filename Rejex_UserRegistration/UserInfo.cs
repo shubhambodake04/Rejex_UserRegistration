f15ce81a454f6c6e425e94fb6eae0460b343a8ec
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-04 19:50:36 +0530

    [Shubham] Add lambda expression

--- Rejex_UserRegistration/UserInfo.cs
+++ Rejex_UserRegistration/UserInfo.cs
@@ -7,19 +7,42 @@ namespace Rejex_UserRegistration
 {
     class UserInfo
     {
-        string FirstName = "^[A-Z][a-z]{2,}$";
-        string LastName = "^[A-Z][a-z]{2,}$";
-        string Email = "^[a-z0-9A-Z]+([._+-@][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
-        string MobileNumber = "^[0-9]{2}[ ][1-9][0-9]{9}$";
-        string Password = "^[a-zA-Z]{8,}$";
-        string PasswordRule2 = "^[A-Z]+[a-zA-Z]{8,}$";
-        string PasswordRule3 = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
+        const string FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        const string LASTNAME = "^[A-Z][a-z]{2,}$";
+        const string EMAIL = "^[a-z0-9A-Z]+([._+-][a-z0-9A-Z]+)*[@][a-z0-9A-Z]+[.][a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        const string MOBILENUMBER = "^[0-9]{2}[ ][1-9][0-9]{9}$";
+        const string PASSWORD_RULE1 = "^[a-zA-Z]{8,}";
+        const string PASSWORD_RULE2 = "[A-Z][a-zA-Z]{7,}";
+        const string PASSWORD_RULE3 = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,20}$";
 
 
+        public Func<string, bool> ValidateFirstName = (input) =>
+        { 
+            return Regex.IsMatch(input, FIRSTNAME); };
 
-        public bool validateUserInfo(string input)
+        public Func<string, bool> ValidateLastName = (input) =>
+        { 
+            return Regex.IsMatch(input, LASTNAME); };
+
+        public Func<string, bool> ValidateEmail = (input) =>
         {
-            return Regex.IsMatch(input, PasswordRule3);
-        }
+            return Regex.IsMatch(input, EMAIL); };
+
+        public Func<string, bool> ValidateMobileNumber = (input) =>
+        {
+            return Regex.IsMatch(input, MOBILENUMBER); };
+
+        public Func<string, bool> ValidatePasswordRule1 = (input) =>
+        {
+            return Regex.IsMatch(input, PASSWORD_RULE1); };
+
+        public Func<string, bool> ValidatePasswordRule2 = (input) =>
+        {
+            return Regex.IsMatch(input, PASSWORD_RULE2); };
+
+        public Func<string, bool> ValidatePasswordRule3 = (input) =>
+        {
+            return Regex.IsMatch(input, PASSWORD_RULE3);};
+
     }
 }
