c3d4cae9c022f8c6a478e001dc2f58b9cb7588de
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-04 19:42:51 +0530

    [Shubham] Add lambda expression

--- Rejex_UserRegistration/Program.cs
+++ Rejex_UserRegistration/Program.cs
@@ -7,9 +7,18 @@ namespace Rejex_UserRegistration
         static void Main(string[] args)
         {
             UserInfo userinfo = new UserInfo();
-            bool result = userinfo.validateUserInfo("ShubhamBodake");
+            bool result = userinfo.ValidateFirstName("Shubham");
             Console.WriteLine(result);
+            bool result1 = userinfo.ValidateLastName("Bodake");
+            Console.WriteLine(result1);
+            bool result2 = userinfo.ValidateEmail("Shubham@gmail.com");
+            Console.WriteLine(result2);
+            bool result3 = userinfo.ValidateMobileNumber("91 7757929383");
+            Console.WriteLine(result3);
+            bool result4 = userinfo.ValidatePasswordRule1("shubhambodake");
+            Console.WriteLine(result4);
             Console.ReadKey();
+
         }
     }
 }
