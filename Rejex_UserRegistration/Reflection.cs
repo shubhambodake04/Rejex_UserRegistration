1c2567c87a133987114a9adef05d07aeb307dcfe
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-10 20:20:39 +0530

    Refactor the code.

--- Rejex_UserRegistration/Reflection.cs
+++ Rejex_UserRegistration/Reflection.cs
@@ -10,27 +10,20 @@ namespace Rejex_UserRegistration
         public static void ReflectionDemo()
         {
             Type type = Type.GetType("Rejex_UserRegistration.UserInfo");
-            Console.WriteLine(type.Name);
-            Console.WriteLine(type.FullName);
+            Console.WriteLine("----Class: " +type.Name);
+            Console.WriteLine("----Class: " + type.FullName);
             Console.WriteLine("Methods in customerInfo class");
             MethodInfo[] methods = type.GetMethods();
             foreach (MethodInfo method in methods)
             {
-                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+                Console.WriteLine("----method:  " + method.Name);
+                ParameterInfo[] parameters = method.GetParameters();
+                foreach(var parm in parameters)
+                {
+                    Console.WriteLine("---Parameter: " +parm.Name + " : " +parm.ParameterType);
+                }
             }
-            Console.WriteLine("Properties in CustomerInfo class");
-            PropertyInfo[] properties = type.GetProperties();
-            foreach (PropertyInfo property in properties)
-            {
-                Console.WriteLine(property.PropertyType.Name + " " + property.Name);
-            }
-
-            Console.WriteLine("Constructors in CustomerInfo class");
-            ConstructorInfo[] constructors = type.GetConstructors();
-            foreach (ConstructorInfo constructor in constructors)
-            {
-                Console.WriteLine(constructor.ToString());
-            }
+           
 
         }
     }
